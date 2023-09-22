918a61f316458ae3b0c5cb1679ac1b072f690165
James Ogbudu <122811319+Jay226-cyber@users.noreply.github.com>
2023-09-23 00:22:58 +0100

    my first python project

---
+++ caculator.py
@@ -0,0 +1,30 @@
+while True:
+     print("1 Addition")
+     print("2 Multiplication")
+     print("3 Subtraction")     
+     print("4 Division")
+     print("Enter Q or q to Exit")
+
+     choice = input("Enter your choice : ")
+
+     if choice == 'Q' or choice == 'q':
+          break
+     num1 = float(input("Enter Number 1 : "))
+     num2 = float(input("Enter Number 2 : "))
+
+     if choice == "1" :
+          print(num1, "+", num2, "=", (num1+num2))
+
+     elif choice == "2" :
+          print(num1, "*", num2, "=", (num1*num2))
+
+     elif choice == "3" :
+          print(num1, "-", num2, "=", (num1-num2))
+
+     elif choice == "4" :
+          if num2 == 0.0:
+               print("Divide by 0 Error")
+          else:
+               print(num1, "/", num2, "=", (num1/num2))
+     else:
+          print("invalid choice")
